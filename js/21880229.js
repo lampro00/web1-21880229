264a6cee2106fd3724a51b895d974fc6dfa0aaf6
lam <you@example.com>
2023-10-13 20:06:36 +0700

    7

---
+++ js/21880229.js
@@ -0,0 +1,11 @@
+const API = "https://web1-api.vercel.app/api";
+const loadData = async (request, templateId, viewId) => {
+  const response = await fetch(`${API}/${request}`);
+  const data = await response.json();
+  console.log(data);
+  var source = document.getElementById(templateId).innerHTML;
+  var template = Handlebars.compile(source);
+  var context = { data: data };
+  var view = document.getElementById(viewId);
+  view.innerHTML = template(context);
+};
