b4c09ef6487307dd0c4e0bedcf66ef0560ae06f1
lam <you@example.com>
2023-10-22 13:04:51 +0700

    8

--- js/21880229.js
+++ js/21880229.js
@@ -9,3 +9,14 @@ const loadData = async (request, templateId, viewId) => {
   var view = document.getElementById(viewId);
   view.innerHTML = template(context);
 };
+const loadData1 = async (request, templateId, viewId) => {
+  const response = await fetch(`${API}/${request}`);
+  const data = await response.json();
+  console.log(data);
+  var source = document.getElementById(templateId).innerHTML;
+  var template = Handlebars.compile(source);
+  var context = { data: data.data };
+  console.log(context);
+  var view = document.getElementById(viewId);
+  view.innerHTML = template(context);
+};
